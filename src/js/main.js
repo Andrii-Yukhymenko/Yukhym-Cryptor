6fc670c9bd8564f16ac2adf50ec5c3fa02c3a521
Andrii <andriy111224@gmail.com>
2021-07-16 19:05:06 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+let encryptedInputText,
+  decryptedInputText,
+  encryptedInputArea = document.querySelector(".enc-input-text"),
+  encryptedInputForm = document.querySelector(".enc-form"),
+  decryptedInputArea = document.querySelector(".dec-input-text"),
+  decryptedInputForm = document.querySelector(".dec-form"),
+  encKey = "",
+  decKey = "",
+  encrypted,
+  decrypted,
+  encResult = document.querySelector(".enc-result"),
+  decResult = document.querySelector(".dec-result");
+
+
+encryptedInputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  encryptedInputText = encryptedInputArea.value;
+  encKey = document.querySelector(".enc-key").value;
+  // console.log(encKey);
+  // console.log(encryptedInputText);
+  encrypted = CryptoJS.AES.encrypt(encryptedInputText, encKey);
+  encrypted = encrypted.toString();
+  // console.log(encrypted);
+  encResult.textContent = encrypted;
+});
+
+decryptedInputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  decryptedInputText = decryptedInputArea.value;
+  decKey = document.querySelector(".dec-key").value;
+  // console.log(decKey);
+  // console.log(decryptedInputText);
+  decrypted = CryptoJS.AES.decrypt(decryptedInputText, decKey);
+  decrypted = decrypted.toString(CryptoJS.enc.Utf8);
+  // console.log(decrypted);
+  decResult.textContent = decrypted;
+});
